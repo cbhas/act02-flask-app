0cc50e1ebea8d98c01be68004042628a25ddef1f
Sebastia <calderonsebastian211@gmail.com>
2025-04-16 09:42:11 -0500

    Update app.py v0.6

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
 from flask import Flask
-from datetime import datetime
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
+
+# Modificar la función home para que lea el archivo https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txtEnlaces a un sitio externo. y en función de ello presentar un tabla HTML con la información de todos las personas que tienen como inicio de ID, los número 3, 4, 5, 7 
 def home():
     url = 'https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt'
-    response = requests.get(url)
-
-    personas = response.text.strip().split('\n')
-    
-    tabla_html = '<table border="1">'
-    tabla_html += '<tr><th>ID</th><th>Nombre</th><th>Edad</th><th>Ciudad</th></tr>'
-    
+    respuesta = requests.get(url)
+    contenido = respuesta.text.splitlines()
+    personas = []
+    for linea in contenido:
+        if linea.startswith(('3', '4', '5', '7')):
+            partes = linea.split(',')
+            persona = {
+                'id': partes[0],
+                'nombre': partes[1],
+                'apellido': partes[2],
+                'edad': partes[3]
+            }
+            personas.append(persona)
+    html = '''
+    <html>
+        <head>
+            <title>Lista de Personas</title>
+        </head>
+        <body>
+            <h1>Lista de Personas</h1>
+            <table border="1">
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Edad</th>
+                </tr>'''
     for persona in personas:
-        datos = persona.split(';')
-        if datos[0][0] in ['3', '4', '5', '7']:
-            tabla_html += f'<tr><td>{datos[0]}</td><td>{datos[1]}</td><td>{datos[2]}</td><td>{datos[3]}</td></tr>'
-    
-    tabla_html += '</table>'
-
-    actual = datetime.now()
-    fecha_formateada = actual.strftime("%d, %B, %Y, %M, %H, %S")
-    return f'<h2>¡Hola Loja, mundo! <b>{fecha_formateada}</b></h2>' + tabla_html
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        html += f'''
+                <tr>
+                    <td>{persona['id']}</td>
+                    <td>{persona['nombre']}</td>
+                    <td>{persona['apellido']}</td>
+                    <td>{persona['edad']}</td>
+                </tr>'''
+    html += '''
+            </table>
+        </body>
+    </html>'''  
